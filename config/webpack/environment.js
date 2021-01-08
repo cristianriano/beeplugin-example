db10647adbd835df0a733580ca00db899bd34cff
Cristian Riano <cristiancarm11@gmail.com>
2021-01-08 15:46:18 +0100

    Render BeePlugin with jQuery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack");
+
+// Avoid using require and import and alias jquery
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+  })
+);
 
 module.exports = environment
